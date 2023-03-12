a66fca2abb586092ac442091798b9f473b271778
Jacob <jaclarsen49@gmail.com>
2023-03-12 15:42:32 -0500

    q3 and q4 done

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// Q1
+
 function additon(num1, num2) {
     return num1 + num2
 }
 
 console.log(additon(3, 5))
 
+//Q2
+
 function hoursIntoSeconds(num) {
     return num * 60 * 60
 }
 
 console.log(hoursIntoSeconds(2))
+
+//Q3
+
+function calcPerimeter(length, width) {
+    return 2 * length + 2 * width
+}
+
+console.log(calcPerimeter(10, 20))
+
+//Q4
+
+function calcTriangleArea(base, height) {
+    return 0.5 * base * height
+}
+
+console.log(calcTriangleArea(3, 2))
