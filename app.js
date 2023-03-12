b3cae850d10c34ef043bca680b31fc38c4cd95ba
Jacob <jaclarsen49@gmail.com>
2023-03-12 15:38:10 -0500

    q1 and q2 done

---
+++ app.js
@@ -0,0 +1,11 @@
+function additon(num1, num2) {
+    return num1 + num2
+}
+
+console.log(additon(3, 5))
+
+function hoursIntoSeconds(num) {
+    return num * 60 * 60
+}
+
+console.log(hoursIntoSeconds(2))
